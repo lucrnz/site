5e33ec647bea2aec940db0446209be37c371808b
lucdev <me@lucdev.net>
2023-01-27 10:56:54 -0300

    Change Xess source url

--- scripts/fetchXess.sh
+++ scripts/fetchXess.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl https://git.lucdev.net/luc/xess/raw/commit/ed032aedcfcd29985bb92365f98b0f6d4a4663b8/xess.css $@
+curl https://raw.githubusercontent.com/lucrnz/Xess/ed032aedcfcd29985bb92365f98b0f6d4a4663b8/xess.css $@
