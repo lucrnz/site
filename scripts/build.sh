f3c72ff5de609e078f5ac6a93b225d2b2c9de75e
lucdev <me@lucdev.net>
2023-02-09 11:54:50 -0300

    Rename js patching script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,4 @@ pnpm exec tsc && \
 cp -av static/scripts public/ && \
 deno run -A scripts/patchRssFeed.ts && \
 deno run -A --unstable scripts/generateJsonFeed.ts && \
-./scripts/addLicenseFieldsToJs.sh
+./scripts/patchScriptFiles.sh
--- scripts/patchScriptFiles.sh
+++ scripts/patchScriptFiles.sh
@@ -8,6 +8,7 @@ for x in $(find "${SCRIPT_DIR}/../public/scripts" -name "*.js"); do
     echo "Adding license to $f"
     mv $f $f.tmp && \
     echo "/* @license magnet:?xt=urn:btih:90dc5c0be029de84e523b9b3922520e79e0e6f08&dn=cc0.txt CC0-1.0 */" >> $f && \
+    echo "/* This file is part of lucdev.net - Visit https://github.com/lucrnz/lucsite for the actual source code */" >> $f && \
     cat $f.tmp >> $f && \
     echo "/* @license-end */" >> $f && \
     rm $f.tmp
