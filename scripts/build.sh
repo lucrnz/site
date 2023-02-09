3a55f8779c26aec4cf6d9c72e7f838e3da97b629
lucdev <me@lucdev.net>
2023-02-09 11:27:06 -0300

    Build script: Add "scripts" folder to bundle

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ zola build && \
 mkdir -p public/fonts && \
 deno run -A scripts/googleFontsToLocal.ts && \
 pnpm exec tsc && \
+cp -av static/scripts public/ && \
 deno run -A scripts/patchRssFeed.ts && \
 deno run -A --unstable scripts/generateJsonFeed.ts && \
 ./scripts/addLicenseFieldsToJs.sh
