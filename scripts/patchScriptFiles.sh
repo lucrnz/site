544aac874825161cebe4deb3b66b1389cff62712
lucdev <me@lucdev.net>
2023-02-09 11:55:12 -0300

    Replace echo message

--- scripts/patchScriptFiles.sh
+++ scripts/patchScriptFiles.sh
@@ -5,7 +5,7 @@ mkdir -p "$(realpath ${SCRIPT_DIR}/../public/scripts)"
 
 for x in $(find "${SCRIPT_DIR}/../public/scripts" -name "*.js"); do
     f=$(realpath $x)
-    echo "Adding license to $f"
+    echo "Patching file $f"
     mv $f $f.tmp && \
     echo "/* @license magnet:?xt=urn:btih:90dc5c0be029de84e523b9b3922520e79e0e6f08&dn=cc0.txt CC0-1.0 */" >> $f && \
     echo "/* This file is part of lucdev.net - Visit https://github.com/lucrnz/lucsite for the actual source code */" >> $f && \
