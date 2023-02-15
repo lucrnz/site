bf12b15f75f039acd3e9731422cacb74f3f1d781
lucdev <me@lucdev.net>
2023-02-14 22:13:50 -0300

    Fix crypto-js load and domready

--- ts/main.ts
+++ ts/main.ts
@@ -1,110 +1,94 @@
 // Anonymous inmediately-executed function to avoid polluting the global scope
-(() => {
+(async () => {
   const getCSSVariableValue = (variableName: string): string =>
     getComputedStyle(document.documentElement).getPropertyValue(
       `--${variableName}`
     );
 
-  document.addEventListener("DOMContentLoaded", () => {
-    //   const mobileBreakpoint = parseInt(getCSSVariableValue("mobile-breakpoint"));
-    const tabletBreakpoint = parseInt(getCSSVariableValue("tablet-breakpoint"));
+  //   const mobileBreakpoint = parseInt(getCSSVariableValue("mobile-breakpoint"));
+  const tabletBreakpoint = parseInt(getCSSVariableValue("tablet-breakpoint"));
 
-    const menuBtn = document.querySelector(
-      "header > nav > .menu-toggle"
-    )! as HTMLAnchorElement;
-    const logo = document.querySelector(
-      "header > nav > .logo"
-    )! as HTMLSpanElement;
-    const navList = document.querySelector("header > nav > ul")!;
-    const nav = document.querySelector("header > nav")!;
+  const menuBtn = document.querySelector(
+    "header > nav > .menu-toggle"
+  )! as HTMLAnchorElement;
+  const logo = document.querySelector(
+    "header > nav > .logo"
+  )! as HTMLSpanElement;
+  const navList = document.querySelector("header > nav > ul")!;
+  const nav = document.querySelector("header > nav")!;
 
-    const adapatElementsResize = () => {
-      const isTabletViewport = window.innerWidth <= tabletBreakpoint;
+  const adapatElementsResize = () => {
+    const isTabletViewport = window.innerWidth <= tabletBreakpoint;
 
-      if (isTabletViewport) {
-        closeMenu(getNavigationLinks());
-      } else {
-        navList.setAttribute("aria-hidden", "false");
-        logo.setAttribute("aria-hidden", "true");
-      }
-    };
-
-    const closeMenu = (links: NodeListOf<HTMLLIElement>) => {
-      navList.setAttribute("aria-hidden", "true");
-      menuBtn.setAttribute("aria-expanded", "false");
-      navList.classList.contains("open") && navList.classList.remove("open");
-      nav.classList.contains("open") && nav.classList.remove("open");
-      links.forEach(
-        (link) =>
-          link.classList.contains("show") && link.classList.remove("show")
-      );
-    };
-
-    const openMenu = (links: NodeListOf<HTMLLIElement>) => {
-      menuBtn.setAttribute("aria-expanded", "true");
-      navList.removeAttribute("aria-hidden");
-      navList.classList.add("open");
-      nav.classList.add("open");
-      links.forEach((link) => link.classList.add("show"));
-    };
-
-    const getNavigationLinks = () =>
-      document.querySelectorAll(
-        "header > nav > ul > li"
-      ) as NodeListOf<HTMLLIElement>;
-
-    menuBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      const expanded = menuBtn.getAttribute("aria-expanded") === "true";
-      const links = getNavigationLinks();
-
-      if (expanded) {
-        closeMenu(links);
-      } else {
-        openMenu(links);
-      }
-    });
-
-    window.addEventListener("resize", () => {
+    if (isTabletViewport) {
       closeMenu(getNavigationLinks());
-      adapatElementsResize();
-    });
+    } else {
+      navList.setAttribute("aria-hidden", "false");
+      logo.setAttribute("aria-hidden", "true");
+    }
+  };
 
+  const closeMenu = (links: NodeListOf<HTMLLIElement>) => {
+    navList.setAttribute("aria-hidden", "true");
+    menuBtn.setAttribute("aria-expanded", "false");
+    navList.classList.contains("open") && navList.classList.remove("open");
+    nav.classList.contains("open") && nav.classList.remove("open");
+    for (const link of links) {
+      link.classList.contains("show") && link.classList.remove("show");
+    }
+  };
+
+  const openMenu = (links: NodeListOf<HTMLLIElement>) => {
+    menuBtn.setAttribute("aria-expanded", "true");
+    navList.removeAttribute("aria-hidden");
+    navList.classList.add("open");
+    nav.classList.add("open");
+    for (const link of links) {
+      link.classList.add("show");
+    }
+  };
+
+  const getNavigationLinks = () =>
+    document.querySelectorAll(
+      "header > nav > ul > li"
+    ) as NodeListOf<HTMLLIElement>;
+
+  menuBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const expanded = menuBtn.getAttribute("aria-expanded") === "true";
+    const links = getNavigationLinks();
+
+    if (expanded) {
+      closeMenu(links);
+    } else {
+      openMenu(links);
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    closeMenu(getNavigationLinks());
     adapatElementsResize();
+  });
 
-    const progressBar = document.querySelector(
-      "header > .progress-bar"
-    )! as HTMLDivElement;
+  adapatElementsResize();
 
-    window.addEventListener("scroll", () => {
-      const totalHeight = document.body.scrollHeight - window.innerHeight;
-      const percentage = parseFloat(
-        ((window.pageYOffset / totalHeight) * 100).toFixed(2)
-      );
+  const progressBar = document.querySelector(
+    "header > .progress-bar"
+  )! as HTMLDivElement;
 
-      if (percentage > 0) {
-        progressBar.style.display = "block";
-        progressBar.style.width = `${percentage}%`;
-      } else {
-        progressBar.style.display = "none";
-        progressBar.style.width = "0";
-      }
-    });
+  window.addEventListener("scroll", () => {
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const percentage = parseFloat(
+      ((window.pageYOffset / totalHeight) * 100).toFixed(2)
+    );
 
-    const interval = setInterval(async () => {
-      if (window.CryptoJS) {
-        console.log(`CryptoJS loaded, starting script...`);
-        clearInterval(interval);
-        try {
-          await decryptProtectedLinks(
-            "qOftmrDIWgroKAZQGxiL130nARSLstcYNwyJaIc2"
-          );
-          await patchExternalLinks();
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    }, 250);
+    if (percentage > 0) {
+      progressBar.style.display = "block";
+      progressBar.style.width = `${percentage}%`;
+    } else {
+      progressBar.style.display = "none";
+      progressBar.style.width = "0";
+    }
   });
 
   const patchExternalLinks = async (): Promise<number> => {
@@ -182,52 +166,70 @@
   const decryptProtectedLinks = async (passphrase: string): Promise<number> => {
     // Rationale for encrypting some links:
     // A lot of bots are crawling the web and are looking for email addresses, phone numbers, etc.
+    // @TODO: Find another library, this one is not maintained anymore.
 
     return new Promise((resolve, reject) => {
-      const debugMode = true;
       const elements = document.querySelectorAll(
         "a[data-protected-link]"
       ) as NodeListOf<HTMLAnchorElement>;
 
-      debugMode &&
-        console.log(`Found ${elements.length} elements to decrypt...`);
-
       if (!elements.length) {
         resolve(0);
+        return;
       }
 
-      elements.forEach((element) => {
-        debugMode && console.log(element);
-        const cryptedStr = element.getAttribute("data-protected-link");
+      const runDecode = (elements: NodeListOf<HTMLAnchorElement>) => {
+        let decodeTotal = 0;
 
-        if (!cryptedStr) {
-          debugMode &&
-            console.error(`No data-protected-link attribute found on element`);
-          return;
+        for (const element of elements) {
+          const cryptedStr = element.getAttribute("data-protected-link");
+
+          if (!cryptedStr) {
+            continue;
+          }
+
+          let decoded: string | undefined;
+          try {
+            decoded = CryptoJS.Rabbit.decrypt(cryptedStr, passphrase).toString(
+              CryptoJS.enc.Utf8
+            );
+          } catch (e) {
+            console.error(`Error decrypting: ${cryptedStr}`);
+            reject(e);
+            return;
+          }
+
+          if (decoded) {
+            element.removeAttribute("href");
+            element.setAttribute("href", decoded);
+            element.removeAttribute("data-protected-link");
+            decodeTotal++;
+          }
         }
 
-        let decoded: string | undefined;
+        resolve(decodeTotal);
+      };
 
-        debugMode && console.log(`Decrypting url: ${cryptedStr}...`);
-
-        try {
-          decoded = CryptoJS.Rabbit.decrypt(cryptedStr, passphrase).toString(
-            CryptoJS.enc.Utf8
-          );
-        } catch (e) {
-          console.error(`Error decrypting: ${cryptedStr}`);
-          reject(e);
-        }
-
-        if (decoded) {
-          debugMode && console.log(`Successfully decrypted url`);
-          element.removeAttribute("href");
-          element.setAttribute("href", decoded);
-          element.removeAttribute("data-protected-link");
-        }
-      });
-
-      resolve(elements.length);
+      // If the script is not loaded yet, load it and then run the decode
+      if (!document.querySelector("#crypto-js-script")) {
+        const script = document.createElement("script");
+        script.id = "crypto-js-script";
+        script.src =
+          "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js";
+        script.setAttribute(
+          "integrity",
+          "sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA=="
+        );
+        script.setAttribute("crossorigin", "anonymous");
+        script.setAttribute("referrerpolicy", "no-referrer");
+        script.onload = () => runDecode(elements);
+        document.head.appendChild(script);
+      } else {
+        runDecode(elements);
+      }
     });
   };
+
+  await decryptProtectedLinks("qOftmrDIWgroKAZQGxiL130nARSLstcYNwyJaIc2");
+  await patchExternalLinks();
 })();
