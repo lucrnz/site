68d56d58dbe82766430b3e6820f00963dfeafd8a
lucdev <me@lucdev.net>
2023-02-15 13:20:31 -0300

    Resume: calculate current experience time

--- ts/main.ts
+++ ts/main.ts
@@ -163,12 +163,44 @@
     });
   };
 
+  interface ExternalScript {
+    id: string;
+    url: string;
+    integrity: string;
+  }
+
+  const externalScripts = {
+    cryptoJs: {
+      id: "crypto-js",
+      url: "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js",
+      integrity: "sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA=="
+    }
+  } as { [key: string]: ExternalScript };
+
+  const requireExternalScript = async (script : ExternalScript): Promise<void> => new Promise<void>((resolve, reject) => {
+      const scriptTagId = `${script.id}-script`;
+      if (document.getElementById(scriptTagId)) {
+        resolve();
+        return;
+      }
+
+      const scriptTag = document.createElement("script");
+      scriptTag.id = scriptTagId;
+      scriptTag.src = script.url;
+      scriptTag.setAttribute("integrity", script.integrity);
+      scriptTag.setAttribute("crossorigin", "anonymous");
+      scriptTag.setAttribute("referrerpolicy", "no-referrer");
+      scriptTag.onload = () => resolve();
+      scriptTag.onerror = () => reject(new Error(`Failed to load script ${script.url}`));
+      document.head.appendChild(scriptTag);
+    })
+
   const decryptProtectedLinks = async (passphrase: string): Promise<number> => {
     // Rationale for encrypting some links:
     // A lot of bots are crawling the web and are looking for email addresses, phone numbers, etc.
     // @TODO: Find another library, this one is not maintained anymore.
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const elements = document.querySelectorAll(
         "a[data-protected-link]"
       ) as NodeListOf<HTMLAnchorElement>;
@@ -178,58 +210,99 @@
         return;
       }
 
-      const runDecode = (elements: NodeListOf<HTMLAnchorElement>) => {
-        let decodeTotal = 0;
+      try {
+        await requireExternalScript(externalScripts.cryptoJs);     
+      } catch(e) {
+        reject(e);
+        return;
+      }
+      
+      let decodeTotal = 0;
+      for (const element of elements) {
+        const cryptedStr = element.getAttribute("data-protected-link");
 
-        for (const element of elements) {
-          const cryptedStr = element.getAttribute("data-protected-link");
-
-          if (!cryptedStr) {
-            continue;
-          }
-
-          let decoded: string | undefined;
-          try {
-            decoded = CryptoJS.Rabbit.decrypt(cryptedStr, passphrase).toString(
-              CryptoJS.enc.Utf8
-            );
-          } catch (e) {
-            console.error(`Error decrypting: ${cryptedStr}`);
-            reject(e);
-            return;
-          }
-
-          if (decoded) {
-            element.removeAttribute("href");
-            element.setAttribute("href", decoded);
-            element.removeAttribute("data-protected-link");
-            decodeTotal++;
-          }
+        if (!cryptedStr) {
+          continue;
         }
 
-        resolve(decodeTotal);
-      };
+        let decoded: string | undefined;
+        try {
+          decoded = CryptoJS.Rabbit.decrypt(cryptedStr, passphrase).toString(
+            CryptoJS.enc.Utf8
+          );
+        } catch (e) {
+          console.error(`Error decrypting: ${cryptedStr}`);
+          reject(e);
+          return;
+        }
 
-      // If the script is not loaded yet, load it and then run the decode
-      if (!document.querySelector("#crypto-js-script")) {
-        const script = document.createElement("script");
-        script.id = "crypto-js-script";
-        script.src =
-          "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js";
-        script.setAttribute(
-          "integrity",
-          "sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA=="
-        );
-        script.setAttribute("crossorigin", "anonymous");
-        script.setAttribute("referrerpolicy", "no-referrer");
-        script.onload = () => runDecode(elements);
-        document.head.appendChild(script);
-      } else {
-        runDecode(elements);
+        if (decoded) {
+          element.removeAttribute("href");
+          element.setAttribute("href", decoded);
+          element.removeAttribute("data-protected-link");
+          decodeTotal++;
+        }
       }
+
+      resolve(decodeTotal);
     });
   };
 
   await decryptProtectedLinks("qOftmrDIWgroKAZQGxiL130nARSLstcYNwyJaIc2");
   await patchExternalLinks();
+
+  if (window.location.pathname.startsWith("/resume")) {
+    console.log("Resume script code");
+
+    interface DateTimeDifference {
+      years: number;
+      months: number;
+      days: number;
+    }
+  
+    // This is from https://stackoverflow.com/a/49201872 - CC-BY-SA 4.0
+    function dateDiff(startDate : Date, endDate : Date) : DateTimeDifference {
+      const startYear = startDate.getFullYear();
+      const february = (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0 ? 29 : 28;
+      const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+      let yearDiff = endDate.getFullYear() - startYear;
+      let monthDiff = endDate.getMonth() - startDate.getMonth();
+      if (monthDiff < 0) {
+        yearDiff--;
+        monthDiff += 12;
+      }
+      let dayDiff = endDate.getDate() - startDate.getDate();
+      if (dayDiff < 0) {
+        if (monthDiff > 0) {
+          monthDiff--;
+        } else {
+          yearDiff--;
+          monthDiff = 11;
+        }
+        dayDiff += daysInMonth[startDate.getMonth()];
+      }
+  
+      return {
+        years: yearDiff,
+        months: monthDiff,
+        days: dayDiff
+      }
+    }
+
+    const experience = document.querySelector("#experience")! as HTMLDivElement;
+    const currentJobTime = experience.querySelector("#resume-job-current-time")! as HTMLSpanElement;
+
+    const {years, months } = dateDiff(new Date(currentJobTime.getAttribute("data-start-time")!), new Date());
+
+    if (years > 0) {
+      if (months > 0) {
+        currentJobTime.innerText = `${years} ${ years == 1 ? "year" : "years" } and ${months} ${ months == 1 ? "month" : "months" }`;
+      } else {
+        currentJobTime.innerText = `${years} ${ years == 1 ? "year" : "years" }`;
+      }
+    } else {
+      currentJobTime.innerText = `${months} months`;
+    }
+  }
 })();
