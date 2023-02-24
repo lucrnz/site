552e3c0a50237ca590fd6b0c3a97891a00cf5a39
lucdev <me@lucdev.net>
2023-02-24 11:47:33 -0300

    Astro: Add robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,25 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+const urlBase = "https://lucdev.net";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://lucdev.net",
-  integrations: [mdx(), sitemap()]
+  site: urlBase,
+  integrations: [
+    mdx(),
+    sitemap(),
+    robotsTxt({
+      sitemap: `{urlBase}/sitemap.xml`,
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+          disallow: ["/404", "/resume"]
+        }
+      ]
+    })
+  ]
 });
