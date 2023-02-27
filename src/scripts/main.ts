c0feb0095ccd64b90e46273da842506b1c559844
lucdev <me@lucdev.net>
2023-02-27 11:13:32 -0300

    Turn off analytics until I figure out GDPR

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,9 @@
 import { SITE_GITHUB } from "../consts";
 import setupHamburgerMenu from "./hamburgerMenu";
-import sendAnalytics from "./sendAnalytics";
+// import sendAnalytics from "./sendAnalytics";
 
 export default async function () {
   console.log(`Psst! Website source code over here: ${SITE_GITHUB}`);
   setupHamburgerMenu();
-  await sendAnalytics();
+  // await sendAnalytics();
 }
