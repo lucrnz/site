eeaf8721213a1554d805e5eb5c8098c9bd421990
lucdev <me@lucdev.net>
2023-04-26 08:44:13 -0300

    enable resume skills

---
+++ src/types/ResumeSkill.ts
@@ -0,0 +1,15 @@
+export enum ResumeSkill {
+  next = "Next.js",
+  react = "React",
+  ts = "TypeScript",
+  js = "JavaScript",
+  csharp = "C#",
+  php = "PHP",
+  dotnet = ".NET",
+  dotnet_legacy = ".NET Framework (legacy)",
+  html = "HTML",
+  css = "CSS",
+  agile = "Agile",
+  solid = "SOLID Design Principles",
+  mssql = "Microsoft SQL Server"
+}
