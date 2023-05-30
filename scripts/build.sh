c4ee3d32b996c61195d677d92bfe8eb1ac3ca1f6
lucdev <me@lucdev.net>
2023-05-30 01:37:20 -0300

    omg work pls

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,5 @@ pnpm exec astro build && \
 test -d dist && \
 find pages -mindepth 1 ! -path 'pages/.git*' -exec rm -fvr {} + && \
 cp -r dist/* pages && \
+cp public/.domains pages/ && \
 test -d dist && rm -rf dist
