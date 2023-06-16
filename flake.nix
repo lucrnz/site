66bcdc436b68caf7a9db9b85c068b12168c89d87
lucdev <me@lucdev.net>
2023-06-16 12:06:21 -0300

    Add nix flake and direnv setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs-18_x
+          pkgs.nodePackages.pnpm
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
