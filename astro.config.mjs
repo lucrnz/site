bec132fff7e029e33e4eacf98783a16462f1fa4b
lucdev <me@lucdev.net>
2024-05-07 08:34:10 -0300

    Prepare for astro-icon v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+// I'll delay updating to Astro Icon v1 because it has a glitch.
+// import astroIcon from "astro-icon";
 
 import robotsTxt from "astro-robots-txt";
 const urlBase = "https://lucdev.net";
