e3ddf4bb064a1cd6a723aff3edf63257c932432b
lucdev <me@lucdev.net>
2024-06-04 16:47:12 -0300

    Prettier format

--- src/scripts/alpinejs.ts
+++ src/scripts/alpinejs.ts
@@ -4,6 +4,6 @@ import intersectPlugin from "@alpinejs/intersect";
 import collapsePlugin from "@alpinejs/collapse";
 
 export default (Alpine: Alpine) => {
-	Alpine.plugin(collapsePlugin);
-	Alpine.plugin(intersectPlugin);
+  Alpine.plugin(collapsePlugin);
+  Alpine.plugin(intersectPlugin);
 };
